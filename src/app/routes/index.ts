7a4aabcf949f85ffe5d7e8f72d8dc1f39f78d51c
Suparna Dhar <suparnad806@gmail.com>
2025-02-24 00:36:36 +0600

    Update index.ts

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,15 +2,16 @@ import { Router } from 'express';
 
 import { AuthRoutes } from '../modules/Auth/auth.route';
 
-
-import { UserRoutes } from '../modules/user/user.route';
+import { UserRoutes } from '../AllRoutes/user.route';
 import { courseRoutes } from '../AllRoutes/course.route';
+import { coursemoduleRoutes } from '../AllRoutes/module.route';
+import { lectureRoutes } from '../AllRoutes/lecture.route';
+
 
 
 const router = Router();
 
 const moduleRoutes = [
-  
   {
     path: '/auth',
     route: AuthRoutes,
@@ -23,10 +24,15 @@ const moduleRoutes = [
     path: '/course',
     route: courseRoutes,
   },
-  
+  {
+    path: '/module',
+    route: coursemoduleRoutes,
+  },
+  {
+    path: '/lecture',
+    route: lectureRoutes,
+  },
 ];
-
-
 
 moduleRoutes.forEach((route) => router.use(route.path, route.route));
 export default router;
